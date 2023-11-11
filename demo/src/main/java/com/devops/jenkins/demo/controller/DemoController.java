dc855eb356c342688a43e574f6a17ba8e97f7fdc
Rajesh Nair <sendthis2raj@gmail.com>
2023-11-11 11:41:41 +0530

    Adding to GitHub.

---
+++ demo/src/main/java/com/devops/jenkins/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.devops.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello! Demo application executed successfully.";
+    }
+}
